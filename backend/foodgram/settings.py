012a986967a00a7bdcad76cadddcdc880790fc7a
Мария Тычинская <tychinskayamariya@gmail.com>
2023-10-15 18:04:06 +0300

    Correct mistakes

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -90,10 +90,10 @@ USE_I18N = True
 USE_TZ = True
 
 STATIC_URL = '/static/'
-STATIC_ROOT = BASE_DIR / 'collected_static'
+STATIC_ROOT = BASE_DIR / 'backend_static/static'
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = BASE_DIR / '/media'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
